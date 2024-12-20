8dcbacb79ddbcadd2d918f8ca0e4015e62260fcb
Alabi Temitope <t.banji@rocketmail.com>
2024-12-20 17:33:27 +0100

    added test case to the project

--- contracts/Ethcommerce.sol
+++ contracts/Ethcommerce.sol
@@ -55,5 +55,45 @@ contract Ethcommerce {
         emit List(_name, _cost, _stock);
     }
 
+    function buy(uint256 _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
+
+
+        // Require enough ether to buy item
+        require(msg.value >= item.cost);
+
+
+        // Require item is in stock
+        require(item.stock > 0);
+
+
+        // Create order
+        Order memory order = Order(block.timestamp, item);
+
+
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+
+        // Subtract stock
+        items[_id].stock = item.stock - 1;
+
+
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
     
+    mapping(address => mapping(uint256 => Order)) public orders;
+ mapping(address => uint256) public orderCount;
+
+ struct Order {
+        uint256 time;
+        Item item;
+  }
+
+  event Buy(address buyer, uint256 orderId, uint256 itemId);
+
+
 }
