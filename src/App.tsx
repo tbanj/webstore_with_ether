133854934c96ab215c756c77625bc09bc30909a5
Alabi Temitope <t.banji@rocketmail.com>
2024-12-20 22:37:02 +0100

    updated the frontend app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+// ABIs
+import Ethcommerce from "./abis/Ethcommerce.json";
+
+// Config
+import config from "./config.json";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [provider, setProvider] = useState<any>(null);
+  const [ethcommerce, setEthcommerce] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const loadedEthcommerce = new ethers.Contract(
+      config[network.chainId].ethcommerce.address,
+      Ethcommerce,
+      provider
+    );
+    console.log(await loadedEthcommerce.items(1));
+    setEthcommerce(loadedEthcommerce);
+
+    const items = [];
+
+    for (var i = 0; i < 9; i++) {
+      const item = await ethcommerce.items(i + 1);
+      items.push(item);
+    }
+    console.log(items, loadedEthcommerce, "hi");
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Ethcommerce Best Sellers</h2>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,51 @@
+import { ethers } from "ethers";
+
+const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.getAddress(accounts[0]);
+      setAccount(account);
+    } catch (error) {
+      if (error.code === 4001) {
+        console.error("User denied connection.");
+      }
+    }
+  };
+
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Ethcommerce</h1>
+      </div>
+
+      <input type="text" className="nav__search" />
+
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+
+      <ul className="nav__links">
+        <li>
+          <a href="#Clothing & Jewelry">Clothing & Jewelry</a>
+        </li>
+        <li>
+          <a href="#Electronics & Gadgets">Electronics & Gadgets</a>
+        </li>
+        <li>
+          <a href="#Toys & Gaming">Toys & Gaming</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
