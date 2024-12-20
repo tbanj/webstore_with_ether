0ae41ddd00c47aa4222a1073caae15748b034e9a
Alabi Temitope <t.banji@rocketmail.com>
2024-12-20 17:37:52 +0100

    implementation of the commerce contract

--- test/ethcommerce.js
+++ test/ethcommerce.js
@@ -64,4 +64,74 @@ describe("Ethcommerce", () => {
       expect(transaction).to.emit(ethcommerce, "List");
     });
   });
+
+  describe("Buying", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      // List a item
+      transaction = await ethcommerce
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      // Buy a item
+      transaction = await ethcommerce.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+    });
+
+    it("Updates buyer's order count", async () => {
+      const result = await ethcommerce.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+
+    it("Adds the order", async () => {
+      const order = await ethcommerce.orders(buyer.address, 1);
+
+      expect(order.time).to.be.greaterThan(0);
+      expect(order.item.name).to.equal(NAME);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(ethcommerce.getAddress());
+      expect(result).to.equal(COST);
+    });
+
+    it("Emits Buy event", () => {
+      expect(transaction).to.emit(ethcommerce, "Buy");
+    });
+  });
+
+  describe("Withdrawing", () => {
+    let balanceBefore;
+
+    beforeEach(async () => {
+      // List a item
+      let transaction = await ethcommerce
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      // Buy a item
+      transaction = await ethcommerce.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+
+      // Get Deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      // Withdraw
+      transaction = await ethcommerce.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates the owner balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(ethcommerce.getAddress());
+      expect(result).to.equal(0);
+    });
+  });
 });
